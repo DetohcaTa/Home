04a59d22ebdca826c0b14910b6b3e2e34ccf7a06
DetohcaTa <tashashahova.ju@gmail.com>
2023-07-27 11:58:03 +0400

    two

---
+++ src/js/component/swiper.js
@@ -0,0 +1,32 @@
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop:true,
+    autoplay: {
+        delay: 3600,
+      },
+ 
+    effect: 'slide',
+   
+
+     observer: true
+
+  });
+  const swiper2 = new Swiper('.swiper2', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop:true,
+    autoplay: {
+        delay: 3600,
+      },
+ 
+    effect: 'slide',
+   
+
+     observer: true
+
+  });
+  
